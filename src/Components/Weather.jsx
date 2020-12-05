add5a7f8d11afd50f9cb21e76ce890b735b025d1
Romarie E. Quinones <romiquin86@romaries-air.attlocal.net>
2020-12-04 21:11:12 -0500

    pushing weather code to ro

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+
 
 function Weather(props) {
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const api_key = 'a62a2a58f29b9102325f286b0be33763';
+
+const fetchWeather = () => {
+    fetch(`${URL}zip=46032,us&appid=${api_key}`, {
+        headers: {
+            'Accept': 'application/json'
+        }}).then(res => res.json())
+            .then(weather => {
+                console.log(weather);
+            })
+}
+
+    useEffect(() => {
+        fetchWeather();
+    }, [])
+
     return (
         <div>
-
+            <h1>Hi</h1>
         </div>
     );
 }
