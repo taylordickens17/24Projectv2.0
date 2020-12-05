6984cb3b0ae019eb7d95875301fda77cd6095f62
TaylorDickens <justindickens17@gmail.com>
2020-12-04 21:12:08 -0500

    began fetch and got back weather information in zip code

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Weather(props) {
+    const URL = 'https://api.openweathermap.org/data/2.5/weather?'
+    const key = 'a62a2a58f29b9102325f286b0be33763'
+
+    const fetchWeather = () => {
+        fetch(`${URL}zip=94040,us&appid=${key}`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+    useEffect(() => {
+        fetchWeather();
+    }, [])
+
     return (
         <div>
-
+            <h1>Oh hi, Mark</h1>
         </div>
     );
 }
